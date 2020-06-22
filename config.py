cc03af4506c31173ab57ff0731a8f282475380f2
imroytran <60065069+imroytran@users.noreply.github.com>
2020-06-22 11:47:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
-BOT_TOKEN = os.environ['BOT_TOKEN']
+BOT_TOKEN = '1182143579:AAEYwVpGe5hEOtJnqOB6M1QlNh7G7sGsHBA'
 
 PROXY_URL = 'socks5://178.128.203.1:1080'
 PROXY_LOGIN = 'student'
 PROXY_PW = 'TH8FwlMMwWvbJF8FYcq0'
 
-WEBHOOK_HOST = os.environ['WEBHOOK_HOST']
+WEBHOOK_HOST = 'https://splendid-strand-280019.uc.r.appspot.com/'
 WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
 WEBHOOK_URL = WEBHOOK_HOST + WEBHOOK_PATH
 
